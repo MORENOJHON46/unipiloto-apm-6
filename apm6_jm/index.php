4d857f0491131a89397c89807ea4d27a1ef01400
JhonMorenoParra <morenojhon@live.com>
2017-07-29 10:32:56 -0500

    creacion de index
    se cargan los archivos del formulario

---
+++ apm6_jm/index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+
+	<meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  	<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
+
+  	<script src="bootstrap/js/bootstrap.js"></script>
+
+	<title>Ejemplo</title>
+</head>
+<body>
+<form action='' method='POST'>
+<div class="container">
+		<table>
+			<tr>
+				<td colspan="2">
+					<h1>Formulario</h1>
+				</td>
+			</tr>
+			<tr>
+				<td>Campo1</td>
+				<td>
+					<input type="text" name="txt1" id="txt1" class="form-control" placeholder="campo 1">
+				</td>
+			</tr>
+			<tr>
+				<td>Campo 2</td>
+				<td>
+					<input type="text" name="txt2" id="txt2" class="form-control" placeholder="campo 2">
+				</td>
+			</tr>
+			<tr>
+				<td>Campo 3</td>
+				<td>
+					<input type="text" name="txt3" id="txt3" class="form-control" placeholder="campo 3">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" class="form-control">
+				</td>
+			</tr>
+		</table>
+		<!--<input type="text" name="txt1" id="txt1" class="form-control" placeholder="campo 1">
+		<input type="text" name="txt2" id="txt2" class="form-control" placeholder="campo 2">
+		<input type="text" name="txt3" id="txt3" class="form-control" placeholder="campo 3">
+		<input type="submit" class="form-control">-->
+</div>
+</form>
+</body>
+</html>
